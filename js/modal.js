6861142aef7ed8ff4ecb04aa5d81b11687eb8b63
Micah Y. Bala <micahkulien@gmail.com>
2019-03-23 17:44:07 +0100

    [Feature #164706118] Added html, styles and javascript for login modal

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,24 @@
 "use strict";
 
 const signUp = document.querySelectorAll(".btn-signup");
-const modal = document.querySelector(".bg-modal");
-const closeModal = document.querySelector(".modal-close");
+const login = document.querySelector(".btn-login");
+const signupModal = document.querySelector(".bg-modal");
+const loginModal = document.querySelector(".bg-login-modal");
+const closeModal = document.querySelectorAll(".modal-close");
 
 signUp.forEach(btn => {
   btn.addEventListener("click", () => {
-    modal.classList.add("modal-active");
+    signupModal.classList.add("modal-active");
   });
 });
 
-closeModal.addEventListener("click", () => {
-  modal.classList.remove("modal-active");
+login.addEventListener("click", () => {
+  loginModal.classList.add("modal-active");
+});
+
+closeModal.forEach(close => {
+  close.addEventListener("click", () => {
+    signupModal.classList.remove("modal-active");
+    loginModal.classList.remove("modal-active");
+  });
 });
