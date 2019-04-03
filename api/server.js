c9c049fbaf8a50a6fa5ac7a5366e94d1bf360665
Micah Y. Bala <micahkulien@gmail.com>
2019-04-03 10:36:40 +0100

    [Feature #164721657] Installed babel and eslint

--- api/server.js
+++ api/server.js
@@ -1,13 +1,15 @@
-const express = require("express");
+import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
-app.get("/", function(req, res) {
-  return res.send("The API endpoint is working!!!");
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => res.send('The API endpoint is working!!!'));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, function() {
-  console.log("Server listening on port 3000...");
+app.listen(PORT, () => {
+  console.log(`Server is running or port ${PORT}`);
 });
