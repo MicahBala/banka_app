290b7759d540c94e5ad77b58df29a82853908cf0
Micah Y. Bala <micahkulien@gmail.com>
2019-04-16 14:53:35 +0100

    [Chore #165375863] Changed user dashboard layout
    Changed the color scheme, dded menu to the sidebar
    And redesigned the transaction table

--- UI/js/AccountModal.js
+++ UI/js/AccountModal.js
@@ -1,29 +1,35 @@
-"use strict";
+'use strict';
 
-const btnNewAccount = document.querySelector(".create-new-account");
-const newAccountModal = document.querySelector(".bg-modal");
-const closeModal = document.querySelector(".modal-close");
+const btnNewAccount = document.querySelector('.create-new-account');
+const newAccountModal = document.querySelector('.bg-modal');
+const closeModal = document.querySelector('.modal-close');
 
-const transactionHistory = document.querySelector(".viewTransaction");
-const transactionModal = document.querySelector(".bg-transaction-modal");
+const transactionHistory = document.querySelector('.viewTransaction');
+const transactionModal = document.querySelector('.bg-transaction-modal');
 const closeTransactionModal = document.querySelector(
-  ".modal-transaction-close"
+  '.modal-transaction-close'
 );
 
+const dashboardMenu = document.querySelector('.dashboard-menu');
+
 // New Account Modal
-btnNewAccount.addEventListener("click", () => {
-  newAccountModal.classList.add("modal-active");
+btnNewAccount.addEventListener('click', () => {
+  newAccountModal.classList.add('modal-active');
+  btnNewAccount.classList.add('active-menu');
+  dashboardMenu.classList.remove('active-menu');
 });
 
-closeModal.addEventListener("click", () => {
-  newAccountModal.classList.remove("modal-active");
+closeModal.addEventListener('click', () => {
+  newAccountModal.classList.remove('modal-active');
+  btnNewAccount.classList.remove('active-menu');
+  dashboardMenu.classList.add('active-menu');
 });
 
 // Transaction Modal
-transactionHistory.addEventListener("click", () => {
-  transactionModal.classList.add("modal-active");
-});
+// transactionHistory.addEventListener('click', () => {
+//   transactionModal.classList.add('modal-active');
+// });
 
-closeTransactionModal.addEventListener("click", () => {
-  transactionModal.classList.remove("modal-active");
+closeTransactionModal.addEventListener('click', () => {
+  transactionModal.classList.remove('modal-active');
 });
