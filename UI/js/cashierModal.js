ddb04872c3297eb5fb5fcc4312f7b03344406ec5
Micah Y. Bala <micahkulien@gmail.com>
2019-04-16 16:44:27 +0100

    [Chore #165375863] Changed the cashier page
    Modified the entire layout and added menu to the sidebar
    Added separate cells for credit and debit account

--- UI/js/cashierModal.js
+++ UI/js/cashierModal.js
@@ -1,31 +1,39 @@
-"use strict";
+'use strict';
 
-const cashierAcctModal = document.querySelector(".bg-cashier-modal");
-const closeCashierAcctModal = document.querySelector(".modal-cashier-close");
+const closeCashierAcctModal = document.querySelector('.modal-cashier-close');
 
-const creditDebitModal = document.querySelector(".bg-credit-debit-modal");
+const creditDebitModal = document.querySelector('.bg-credit-debit-modal');
 const closeCreditDebitAcctModal = document.querySelector(
-  ".modal-credit-debit-close"
+  '.modal-credit-debit-close'
 );
 
-const accountHistory = document.querySelector(".acct-view");
-const creditDebitAccount = document.querySelector(".credit-debit");
+const creditAccount = document.querySelector('.credit-account');
+const debitAccount = document.querySelector('.debit-account');
 
-// Transaction History Modal
-accountHistory.addEventListener("click", () => {
-  cashierAcctModal.classList.add("modal-active");
-  console.log(cashierAcctModal);
+const transactionTitle = document.querySelector('.credit-account-title');
+const creditAmount = document.getElementById('credit-amount-label');
+const creditBtn = document.getElementById('credi-account-btn');
+
+// Credit Account Modal
+creditAccount.addEventListener('click', () => {
+  creditDebitModal.classList.add('modal-active');
 });
 
-closeCashierAcctModal.addEventListener("click", () => {
-  cashierAcctModal.classList.remove("modal-active");
+closeCreditDebitAcctModal.addEventListener('click', () => {
+  creditDebitModal.classList.remove('modal-active');
 });
 
-// Credit/Debit Account Modal
-creditDebitAccount.addEventListener("click", () => {
-  creditDebitModal.classList.add("modal-active");
+// Debit Account Modal
+debitAccount.addEventListener('click', () => {
+  creditDebitModal.classList.add('modal-active');
+  transactionTitle.innerText = 'DEBIT ACCOUNT';
+  creditAmount.innerText = 'AMOUNT TO DEBIT';
+  creditBtn.innerText = 'Debit Account';
 });
 
-closeCreditDebitAcctModal.addEventListener("click", () => {
-  creditDebitModal.classList.remove("modal-active");
+closeCreditDebitAcctModal.addEventListener('click', () => {
+  creditDebitModal.classList.remove('modal-active');
+  transactionTitle.innerText = 'CREDIT ACCOUNT';
+  creditAmount.innerText = 'AMOUNT TO CREDIT';
+  creditBtn.innerText = 'Credit Account';
 });
