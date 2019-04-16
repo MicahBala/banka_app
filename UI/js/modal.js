ad21dd770173606f6e0ff22c657ae407075e26ac
Micah Y. Bala <micahkulien@gmail.com>
2019-04-16 11:45:13 +0100

    [Chore #165375863] Added redirect scripts to login and signup forms

--- UI/js/modal.js
+++ UI/js/modal.js
@@ -1,24 +1,34 @@
-"use strict";
+'use strict';
 
-const signUp = document.querySelectorAll(".btn-signup");
-const login = document.querySelector(".btn-login");
-const signupModal = document.querySelector(".bg-modal");
-const loginModal = document.querySelector(".bg-login-modal");
-const closeModal = document.querySelectorAll(".modal-close");
+const signUp = document.querySelectorAll('.btn-signup');
+const login = document.querySelector('.btn-login');
+const signupModal = document.querySelector('.bg-modal');
+const loginModal = document.querySelector('.bg-login-modal');
+const closeModal = document.querySelectorAll('.modal-close');
+const formSignUp = document.querySelector('.formBtnSignup');
+const formLogin = document.querySelector('.formBtnLogin');
 
 signUp.forEach(btn => {
-  btn.addEventListener("click", () => {
-    signupModal.classList.add("modal-active");
+  btn.addEventListener('click', () => {
+    signupModal.classList.add('modal-active');
   });
 });
 
-login.addEventListener("click", () => {
-  loginModal.classList.add("modal-active");
+login.addEventListener('click', () => {
+  loginModal.classList.add('modal-active');
 });
 
 closeModal.forEach(close => {
-  close.addEventListener("click", () => {
-    signupModal.classList.remove("modal-active");
-    loginModal.classList.remove("modal-active");
+  close.addEventListener('click', () => {
+    signupModal.classList.remove('modal-active');
+    loginModal.classList.remove('modal-active');
   });
+});
+
+formSignUp.addEventListener('click', () => {
+  window.location.href = 'user-dashboard.html';
+});
+
+formLogin.addEventListener('click', () => {
+  window.location.href = 'cashier.html';
 });
